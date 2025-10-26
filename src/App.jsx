12f7418bf395f21731b6f6cddfc723b9aa22d94e
Zoha Kashif <nakachenx@gmail.com>
2025-10-27 04:52:06 +0530

    agh

--- src/App.jsx
+++ src/App.jsx
@@ -2466,7 +2466,7 @@ function Desktop({ apps, onOpenApp }) {
   // Generate stars once on mount
   useEffect(() => {
     const generateStars = () => {
-      return Array.from({ length: 15 }, (_, i) => ({
+      return Array.from({ length: 5 }, (_, i) => ({
         id: i,
         left: Math.random() * 100,
         top: Math.random() * 100,
